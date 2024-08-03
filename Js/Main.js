9953e2cc82d7123be6f76ccbe79e544fa28f908c
mohamed sayed <94808133+iskepr@users.noreply.github.com>
2024-08-03 23:55:15 +0300

    انشاء المشروع وعمل تصميم مبدئ لصفحه البدايه

---
+++ Js/Main.js
@@ -0,0 +1,12 @@
+fetch("Imgs/animation.json")
+  .then((response) => response.json())
+  .then((data) => {
+    // تهيئة الرسوم المتحركة باستخدام Lottie
+    lottie.loadAnimation({
+      container: document.getElementById("animationContainer"), // عنصر الـ div الذي سيتم عرض الرسوم المتحركة فيه
+      renderer: "svg",
+      loop: true,
+      autoplay: true,
+      animationData: data, // بيانات الرسوم المتحركة من ملف JSON
+    });
+  });
