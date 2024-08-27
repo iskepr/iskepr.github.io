d7879b25d392717c34063fcbf2caa928230789f7
mohamed sayed <94808133+iskepr@users.noreply.github.com>
2024-08-28 00:10:39 +0300

    اضافه صفحه ال404 و اضافه التحميل لمهايه الصفحه و اشياء اخري

--- Js/Main.js
+++ Js/Main.js
@@ -32,9 +32,12 @@ function copyEmail() {
   }, 2000);
 }
 
+
+
+
 // animation contact
 let startSection = document.querySelector(".start");
-let workSection = document.querySelector(".wordk");
+let workSection = document.querySelector(".work");
 let storySection = document.querySelector(".story");
 let contactSection = document.querySelector(".contact");
 
@@ -42,37 +45,53 @@ let meimg = document.querySelector(".meimg");
 let metext = document.querySelector(".me .text");
 let me = document.querySelector(".me");
 
+let prograsbarin = document.querySelector(".prograsbarin");
+let prograsbar = document.querySelector(".progras");
+
 window.onscroll = function () {
   let value = window.scrollY;
   let width = window.screen.availWidth;
   meimg.style.top = value * 0.9 + "px";
   metext.style.bottom = value + "px";
 
-  if (value >= 100 && width < 700) {
+  prograsbarin.style.width = value / 20.7 + "%";
+
+  if (value >= 100 && width < 1000) {
     metext.style.opacity = 0;
   } else {
     metext.style.opacity = 1;
   }
   if (width > 700) {
-    me.style.top = value * .4 + "px";
+    me.style.top = value * 0.4 + "px";
   }
 
-  if (window.scrollY >= workSection.offsetTop) {
-    contactSection.style.display = "none";
-    startSection.style.display = "none";
-    workSection.style.display = "block";
-    storySection.style.display = "none";
-  }
-  if (window.scrollY >= storySection.offsetTop) {
-    contactSection.style.display = "none";
-    startSection.style.display = "none";
-    workSection.style.display = "none";
-    storySection.style.display = "block";
-  }
-  if (window.scrollY >= contactSection.offsetTop) {
-    contactSection.style.display = "block";
-    startSection.style.display = "none";
-    workSection.style.display = "none";
-    storySection.style.display = "none";
+  if (window.scrollY >= contactSection.offsetTop - 300) {
+    contactSection.style.opacity = 1;
+    startSection.style.opacity = 0;
+    workSection.style.opacity = 0;
+    storySection.style.opacity = 0;
+    prograsbarin.style.width = 100 + "%";
+    prograsbar.style.opacity = 0;
+    console.log("تم contact الاتصال بنجاح");
+  } else if (window.scrollY >= storySection.offsetTop - 200) {
+    contactSection.style.opacity = 0;
+    startSection.style.opacity = 0;
+    workSection.style.opacity = 0;
+    storySection.style.opacity = 1;
+    prograsbar.style.opacity = 1;
+
+    console.log("تم story المقالات بنجاح");
+  } else if (window.scrollY >= workSection.offsetTop - 300) {
+    contactSection.style.opacity = 0;
+    startSection.style.opacity = 0;
+    workSection.style.opacity = 1;
+    storySection.style.opacity = 0;
+    console.log("تم work المشاريع بنجاح");
+  } else if (window.scrollY >= startSection.offsetTop + 200) {
+    contactSection.style.opacity = 0;
+    startSection.style.opacity = 1;
+    workSection.style.opacity = 0;
+    storySection.style.opacity = 0;
+    console.log("تم start الصفحة الرئيسية بنجاح");
   }
 };
