88782a74be134bb748cde7d076c827aac28cc2bd
مُحَمّد سِيّد <94808133+iskepr@users.noreply.github.com>
2025-09-04 20:16:09 +0300

    تجهيز المشروع

---
+++ src/app/layout.js
@@ -0,0 +1,24 @@
+import { IBM_Plex_Sans_Arabic } from "next/font/google";
+import "./globals.css";
+
+const ibm = IBM_Plex_Sans_Arabic({
+    variable: "--font-ibm-sans",
+    subsets: ["latin", "arabic"],
+    weight: ["100", "200", "300", "400", "500", "600", "700"],
+});
+
+export const metadata = {
+    title: "مُحمد سَــــــــيد",
+    description:
+        "السلام عليكم انا مُحمد سيد مُطور مواقع منذ 1442 هـ ومُطور تطبيقات باستخدام فلاتر منذ 1445 هـ",
+    keywords:
+        "محمد سيد , محمد سيد عبدالعليم , محمد سيد عبدالعليم محمود , سكيبر , محمد سكيبر , محمد سيد سكيبر , مصر , الزاوية الحمرء ,محمد سيد حاج , محمد السيد , محد سيد ازاوية الحمراء , Mohamed , Mohamed Sayed , Mohamed Skepr , Mohamed Sayed Skepr , Mohamed Sayed AbdElalem ,iskrpr , itskepr, qwsdxccc",
+};
+
+export default function RootLayout({ children }) {
+    return (
+        <html lang="ar">
+            <body className={`${ibm.variable}`}>{children}</body>
+        </html>
+    );
+}
---
+++ src/app/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <div>
+            <h1>السلام عليكم</h1>
+        </div>
+    );
+}
